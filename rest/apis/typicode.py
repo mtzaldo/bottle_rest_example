dd91e4d30bdc471293a8b2d94512bdf38acaa9b2
aldo <martinez.aldo@gmail.com>
2020-06-23 22:35:00 -0500

    Removed flake8 linting warnings

--- rest/apis/typicode.py
+++ rest/apis/typicode.py
@@ -1,39 +1,41 @@
 import requests
 
+
 class TypicodeTodoApiClient:
-    
+
     PATH = 'todos'
-    
+
     def __init__(self, base_uri):
         self.base_uri = base_uri
-        
+
     def all(self):
         uri = f'{self.base_uri}/{self.PATH}'
         res = requests.get(uri)
-        
+
         return res.text if res.ok else []
-    
+
     def get(self, id):
         uri = f'{self.base_uri}/{self.PATH}/{id}'
         res = requests.get(uri)
-        
+
         return res.json() if res.ok else {}
-        
+
+
 class TypicodePostApiClient:
 
     PATH = 'posts'
-    
+
     def __init__(self, base_uri):
         self.base_uri = base_uri
-        
+
     def all(self):
         uri = f'{self.base_uri}/{self.PATH}'
         res = requests.get(uri)
-        
+
         return res.text
-    
+
     def get(self, id):
         uri = f'{self.base_uri}/{self.PATH}/{id}'
         res = requests.get(uri)
-        
+
         return res.json()
