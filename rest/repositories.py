ec414500a2424c236099c77905a9fc1dca8cff9d
aldo <martinez.aldo@gmail.com>
2020-06-23 22:32:43 -0500

    Removed flake8 linting warnings

--- rest/repositories.py
+++ rest/repositories.py
@@ -1,51 +1,60 @@
 import sqlite3
 
+
 class WordsRepository:
-    
+
     def __init__(self, dbfile):
         self.dbfile = dbfile
-        
+
     def get(self, word):
-        
+
         db = sqlite3.connect(self.dbfile)
         db.row_factory = sqlite3.Row
-        
+
         row = None
-        
+
         try:
-            row = db.execute('SELECT replacement FROM replacements WHERE word = ?;', (word,)).fetchone()
-            #if autocommit: db.commit()
+            row = \
+                db.execute(
+                    'SELECT replacement FROM replacements WHERE word = ?;',
+                    (word,)
+                ).fetchone()
+            # if autocommit: db.commit()
         except sqlite3.IntegrityError:
             db.rollback()
-            #raise HTTPError(500, "Database Error", e)
+            # raise HTTPError(500, "Database Error", e)
         finally:
             db.close()
-        
+
         return row['replacement'] if row else None
-        
+
+
 class UsersRepository:
-    
+
     def __init__(self, db):
         self.db = db
-        
+
     def get(self, username, password):
-        
-        iter = filter(
-                lambda u: u.get('username') == username and u.get('password') == password, 
+
+        iter = \
+            filter(
+                lambda u:
+                    u.get('username') == username and
+                    u.get('password') == password,
                 self.db.users
             )
-            
+
         user = next((u for u in iter), {})
-        
+
         return user.copy()
-        
+
     def get_by_id(self, id):
-        
+
         iter = filter(
-                lambda u: u.get('id', 0) == id, 
+                lambda u: u.get('id', 0) == id,
                 self.db.users
             )
-        
+
         user = next((u for u in iter), {})
-        
+
         return user.copy()
