9b659582b171a738b0399e1f5e6da68d367bef5e
aldo <martinez.aldo@gmail.com>
2020-06-23 22:27:26 -0500

    Removed flake8 linting warnings

--- rest/apps.py
+++ rest/apps.py
@@ -1,38 +1,42 @@
-from bottle import Bottle, run, request, response
+from bottle import Bottle, request, response
 
 from services import TodoService, PostService, UserService
 from endpoints import TodoEndpoints, PostEndpoints, UserEndpoints
 from repositories import UsersRepository, WordsRepository
 from apis.typicode import TypicodeTodoApiClient, TypicodePostApiClient
 
-import db, settings
+import db
+import settings
+
 
 class TodoApp(Bottle):
-    
+
     def __init__(self):
         super().__init__()
         api = TypicodeTodoApiClient(settings.TYPECODE_URI)
         users_repo = UsersRepository(db)
-        #repository handling the db open/close itself
+        # repository handling the db open/close itself
         words_repo = WordsRepository(settings.REPLACEMENTS_DB)
         service = TodoService(api, users_repo, words_repo)
-        
+
         TodoEndpoints(self, response, service)
-                
+
+
 class PostApp(Bottle):
-    
+
     def __init__(self):
         super().__init__()
         api = TypicodePostApiClient(settings.TYPECODE_URI)
         service = PostService(api)
-        
+
         PostEndpoints(self, service)
 
+
 class UserApp(Bottle):
+
     def __init__(self):
         super().__init__()
         repo = UsersRepository(db)
         service = UserService(repo)
-        
+
         UserEndpoints(self, request, response, service)
-        
