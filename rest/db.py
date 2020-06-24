7d6a01072b39ae7084f10b1b0495bdecc72700d3
aldo <martinez.aldo@gmail.com>
2020-06-23 22:20:17 -0500

    Removed flake8 linting warnings

--- rest/db.py
+++ rest/db.py
@@ -1,13 +1,13 @@
 users = [{
-    'id':1,
+    'id': 1,
     'username': 'user1',
     'password': 'password',
 }, {
-    'id':2,
+    'id': 2,
     'username': 'user2',
     'password': 'password',
 }, {
-    'id':3,
+    'id': 3,
     'username': 'user3',
     'password': 'password',
 }]
